a47e474ef7239d56fa74bf67796d67b7624be028
R1chardvieira <luckasrichard22@gmail.com>
2025-07-08 19:15:52 -0300

    First commit

---
+++ AppClientes/Cliente.cs
@@ -0,0 +1,14 @@
+using System.Data.Common;
+
+namespace Cadastro
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public DateTime CadastradoEm { get; set; }
+        public decimal Desconto { get; set; }
+    
+    }
+}
